830bd12167079732bb0a3052e6d96d5bd9e09709
Jose Mari Victorio G. Genuino <jose_mari_genuino@dlsu.edu.ph>
2023-04-20 19:06:07 +0800

    Revert "backend"
    This reverts commit 1f9113e54616ce3da1ba681d7e926e666d43d381.

--- src/flask-server/server.py
+++ src/flask-server/server.py
@@ -5,14 +5,7 @@ app = Flask(__name__)
 
 @app.route("/hello")
 def hello():
-<<<<<<< Updated upstream
-=======
-    print("" + request.args.get('contentUrl'))
-    print(request.args.get('styleUrl'))
->>>>>>> Stashed changes
     return {"stringout":"hello world"}
-
-#
 
 if __name__ == "__main__":
     app.run(debug=True)
